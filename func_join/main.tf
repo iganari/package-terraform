a3677483912c85d05fd5e56a6426a0199946ea3f
iganari <cigaguri@gmail.com>
2020-05-18 09:57:59 +0900

    add func main

---
+++ func_join/main.tf
@@ -0,0 +1,11 @@
+output "join-01" {
+  value = join(",", ["foo", "bar", "baz"])
+}
+
+output "join-02" {
+  value = join(",", ["foo"])
+}
+
+output "join-03" {
+  value = join("-", ["foo", "bar", "baz"])
+}
