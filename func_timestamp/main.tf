40cc33f85752803efb6298639f919536e8e6da7f
iganari / Toru Igarashi <cigaguri@gmail.com>
2022-02-26 13:25:03 +0900

    Update main.tf

--- func_timestamp/main.tf
+++ func_timestamp/main.tf
@@ -2,6 +2,7 @@ output "timestamp" {
   value = timestamp()
 }
 
+# https://www.terraform.io/language/functions/formatdate
 output "format_timestamp" {
   value = formatdate("YYYY-MM-DD-hh-mm", timestamp())
 }
